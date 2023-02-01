298c11ca7357ae73341eed052407c9e8846ebcb7
shaaheenam <69210229+shaaheenam@users.noreply.github.com>
2023-02-01 17:26:31 +0530

    created a new app for xml conversion, added author's linkedin and email links to post_details.html

---
+++ xmlConv/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Post
+
+import xml.etree.ElementTree as ET
+from collections import defaultdict
+
+# Create your views here.
+
+#class to get the xml file
+
+#class to convert xml to defaultdict
+
+#
