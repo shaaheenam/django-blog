5fbcd00cd817672a7c3f27d622bb0c2802e5f697
shaaheenam <69210229+shaaheenam@users.noreply.github.com>
2023-02-06 21:12:20 +0530

    Created a different app for XML conversion functionality. Only started the project, functionality yet to be added.

--- xmlConv/models.py
+++ xmlConv/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+# static database model
+
+class Files(models.Model):
+    xmlfile = models.FileField(upload_to='files/')
+
+    def get_absolute_url(self):
+        return reverse('xmltopdf')
+
+class Customers(models.Model):
+
+    customerID = models.CharField(max_length=200, blank=True)
+    companyName = models.CharField(max_length=200, blank=True)
+    contactName = models.CharField(max_length=200, blank=True)
+    contactTitle = models.CharField(max_length=200, blank=True)
+    phone = models.CharField(max_length=200, blank=True)
+    fax = models.CharField(max_length=200, blank=True)
+    address = models.CharField(max_length=200, blank=True)
+    city = models.CharField(max_length=200, blank=True)
+    region = models.CharField(max_length=200, blank=True)
+    postalCode = models.IntegerField()
+    country = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.contactName
+
+
+class Orders(models.Model):
+
+    customerID = models.ForeignKey(Customers, on_delete=models.CASCADE)
+    employeeID = models.IntegerField()
+    orderDate = models.DateTimeField()
+    requiredDate = models.DateTimeField()
+    shippedDate = models.DateTimeField(null=True)
+    shipVia = models.IntegerField()
+    freight = models.FloatField()
+    shipName = models.CharField(max_length=200, blank=True)
+    shipAddress = models.CharField(max_length=200, blank=True)
+    shipCity = models.CharField(max_length=200, blank=True)
+    shipRegion = models.CharField(max_length=200, blank=True)
+    shipPostalCode = models.IntegerField()
+    shipCountry = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.shipName
+
+
+# need to create a dynamic model later
--- xmlConv/views.py
+++ xmlConv/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Customers, Orders, Files
+from django.views.generic import CreateView
 
 import xml.etree.ElementTree as ET
 from collections import defaultdict
@@ -7,6 +8,11 @@ from collections import defaultdict
 # Create your views here.
 
 #class to get the xml file
+class XMLtoTable(CreateView):
+    model = Files
+    template_name = 'xmlConv/xmltopdf.html'
+    fields = ['xmlfile']
+
 
 #class to convert xml to defaultdict
 
